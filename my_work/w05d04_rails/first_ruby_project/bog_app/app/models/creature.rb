8a433a09bbf01b55b07d5e447cab6f12dafeadd1
bradfarquhar <farquharbradley@gmail.com>
2015-09-22 08:58:49 -0700

    bog app

---
+++ my_work/w05d04_rails/first_ruby_project/bog_app/app/models/creature.rb
@@ -0,0 +1,4 @@
+class Creature < ActiveRecord::Base
+	belongs_to :creature
+    has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+end
---
+++ my_work/w05d04_rails/first_ruby_project/bog_app/config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+    root to: 'creatures#index' # same as `get "/" => "creatures#index"`
+    get '/creatures', to: 'creatures#index', as: 'creatures' # same as `get "/creatures" => "creatures#index"`
+    get '/creatures/new', to: 'creatures#new', as: "new_creature"
+    get '/creatures/:id', to: 'creatures#show', as: "creature"
+    get '/creatures/:id/edit', to: 'creatures#edit', as: "edit_creature"  
+    post '/creatures', to: 'creatures#create'
+    # The update route
+    patch "/creatures/:id", to: "creatures#update"
+    # the destroy route
+    delete "/creatures/:id", to: "creatures#destroy"
+end
